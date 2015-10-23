767dadbd066be37dc2fd2bffb5e0ebd2009ed24c
Luiz Ricardo Vizentini <lrvizentini@gmail.com>
2015-10-22 22:31:06 -0200

    Código de exemplo spbre HttpHandlers

---
+++ CustomHttpHandler/Configuration/ImageConverterSection.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomHttpHandler.Configuration
+{
+    public class ImageConverterSection : ConfigurationSection
+    {
+        [ConfigurationProperty("useWatermark", DefaultValue = "true")]
+        public bool UseWatermark
+        {
+            get
+            {
+                return (bool)this["useWatermark"];
+            }
+            set
+            {
+                this["useWatermark"] = value;
+            }
+        }
+
+        [ConfigurationProperty("watermarkText")]
+        public string WatermarkText
+        {
+            get
+            {
+                return (string)this["watermarkText"];
+            }
+            set
+            {
+                this["watermarkText"] = value;
+            }
+        }
+    }
+}
---
+++ CustomHttpHandler/CustomHandlers/ImageConverterHandler.cs
@@ -0,0 +1,80 @@
+﻿using CustomHttpHandler.Configuration;
+using System.Configuration;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Web;
+
+namespace CustomHttpHandler.CustomHandlers
+{
+    public class ImageConverterHandler : System.Web.IHttpHandler
+    {
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            using (Bitmap bmap = new Bitmap(context.Request.PhysicalPath))
+            {
+                var image = WriteTextOnImage(context, bmap);
+
+                var isMobile = context.Request.Browser.IsMobileDevice;
+
+                if (isMobile)
+                {
+                    context.Response.ContentType = "image/gif";
+                    image.Save(context.Response.OutputStream,
+                        ImageFormat.Gif);
+                }
+                else
+                {
+                    context.Response.ContentType = "image/jpeg";
+                    image.Save(context.Response.OutputStream,
+                        ImageFormat.Jpeg);
+                }
+            }
+        }
+
+        private Bitmap WriteTextOnImage(HttpContext context, Bitmap image)
+        {
+            ImageConverterSection config =
+                (ImageConverterSection)
+                    ConfigurationManager.GetSection("imageConverter");
+
+            if (!config.UseWatermark)
+                return image;
+
+            Bitmap tempBitmap = new Bitmap(image.Width, image.Height);
+
+            using (Graphics gphx = Graphics.FromImage(tempBitmap))
+            {
+                gphx.DrawImage(image, 0, 0);
+
+                Font fontWatermark =
+                    new Font("Verdana", 12, FontStyle.Italic);
+
+                StringFormat stringFormat = new StringFormat();
+                stringFormat.Alignment = StringAlignment.Center;
+                stringFormat.LineAlignment = StringAlignment.Center;
+
+                var watermarkText = config.WatermarkText;
+
+                if (string.IsNullOrWhiteSpace(watermarkText))
+                    watermarkText = " - Protected - ";
+
+                gphx.DrawString(watermarkText, fontWatermark,
+                    Brushes.Red,
+                    new Rectangle(10, 10,
+                    image.Width - 10,
+                    image.Height - 10), stringFormat);
+
+                return tempBitmap;
+            }
+        }
+
+    }
+}
